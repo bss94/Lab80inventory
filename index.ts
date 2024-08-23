564dfe39978f635b95735f2571959927bd132648
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-08-23 14:53:16 +0600

    step 2:complete index multer config types, create server on index, create types Item category Place and typewithoutId for them create type DataBase

---
+++ index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import fileDb from './fileDb';
+import cors from 'cors';
+import config from './config';
+
+const app = express();
+const port = 8000;
+
+app.use(cors(config.corsOptions));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/categories', categoriesRouter);
+app.use('/places', placesRouter);
+app.use('/items', itemsRouter);
+
+const run = async () => {
+  await fileDb.init();
+
+  app.listen(port, () => {
+    console.log(`Server started on ${port} port!`);
+  });
+};
+
+run().catch(console.error);
+
---
+++ types.ts
@@ -0,0 +1,31 @@
+export interface Item{
+id: string;
+categoryId: string;
+placeId: string;
+title: string;
+description: string;
+photo:string | null;
+date: string;
+}
+
+export interface Category{
+id: string;
+title: string;
+description: string;
+}
+
+export interface Place{
+  id: string;
+  title: string;
+  description: string;
+}
+
+export type ItemWithoutId = Omit<Item, "id">;
+export type CategoryWithoutId = Omit<Category, "id">;
+export type PlaceWithoutId = Omit<Place, "id">;
+
+export interface DataBase{
+  places:Place[];
+  categories:Category[];
+  items:Item[];
+}
